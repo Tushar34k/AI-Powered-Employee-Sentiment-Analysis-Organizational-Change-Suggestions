b05cd13955a075f463a3c25b26891d2149be66cd
tushar <tusharkokane366@gmail.com>
2025-02-04 20:17:43 +0530

    initial commit

---
+++ AiPowerd/src/main/java/com/AiPowerd/AiPowerd/AiPowerdApplication.java
@@ -0,0 +1,14 @@
+package com.AiPowerd.AiPowerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPowerdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPowerdApplication.class, args);
+		System.out.println("open ai starts");
+	}
+
+}
---
+++ AiPowerd/src/test/java/com/AiPowerd/AiPowerd/AiPowerdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AiPowerd.AiPowerd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPowerdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
